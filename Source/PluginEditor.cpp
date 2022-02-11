3e85a6516291acabe868914235b8c20fd37efc55
Dylan <61366167+flubber2077@users.noreply.github.com>
2022-02-11 11:05:42 -0500

    Added in eq controls in gui, need to figure out how i want to move forward on implementing those.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,9 +15,11 @@ PanOFlexAudioProcessorEditor::PanOFlexAudioProcessorEditor (PanOFlexAudioProcess
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 250);
+    setSize (550, 200);
 
     setSliderParams(volumeSlider);
+    setSliderParams(bassSlider);
+    setSliderParams(trebleSlider);
     setSliderParams(masterSlider);
     setSliderParams(reverbSlider);
 
@@ -37,32 +39,42 @@ void PanOFlexAudioProcessorEditor::paint (juce::Graphics& g)
     g.setColour (juce::Colours::white);
     g.setFont (22.0f);
 
-    int numControls = 3;
+    auto area = getLocalBounds();
+    area = area.withTop(area.getBottom() - 40);
 
-    auto box = getLocalBounds().reduced(10);
-    box = box.withTop(box.getBottom() - 40);
+    const auto width = area.getWidth() / numControls;
 
-    const auto width = box.getWidth() / numControls;
-
-    g.drawFittedText(TRANS("Volume"), box.removeFromLeft(width), juce::Justification::centred, 1);
-    g.drawFittedText(TRANS("Master"), box.removeFromLeft(width), juce::Justification::centred, 1);
-    g.drawFittedText(TRANS("Reverb"), box.removeFromLeft(width), juce::Justification::centred, 1);
+    g.drawFittedText(TRANS("Volume"), area.removeFromLeft(width), juce::Justification::centred, 1);
+    g.drawFittedText(TRANS("Bass"), area.removeFromLeft(width), juce::Justification::centred, 1);
+    g.drawFittedText(TRANS("Treble"), area.removeFromLeft(width), juce::Justification::centred, 1);
+    g.drawFittedText(TRANS("Master"), area.removeFromLeft(width), juce::Justification::centred, 1);
+    g.drawFittedText(TRANS("Reverb"), area.removeFromLeft(width), juce::Justification::centred, 1);
 }
 
 void PanOFlexAudioProcessorEditor::resized()
 {
-    auto box = getLocalBounds().reduced(10);
+    auto area = getLocalBounds();
+    area.removeFromBottom(40);
 
-    box.removeFromBottom(40);
+    auto bannerArea = area.removeFromTop(30);
+    auto buttonArea = area.removeFromTop(30);
 
-    const auto width = box.getWidth() / 3;
+    auto knobArea = area;
 
-    int padding = 10;
+    const auto knobWidth = knobArea.getWidth() / numControls;
 
-    volumeSlider.setBounds(box.removeFromLeft (width).reduced (padding));
-    brightButton.setBounds(volumeSlider.getLocalBounds().reduced(padding));
-    masterSlider.setBounds(box.removeFromLeft(width).reduced(padding));
-    reverbSlider.setBounds(box.removeFromLeft(width).reduced(padding));
+    int padding = 0;
+
+    volumeSlider.setBounds(knobArea.removeFromLeft (knobWidth).reduced (padding));
+    bassSlider.setBounds(knobArea.removeFromLeft(knobWidth).reduced(padding));
+    trebleSlider.setBounds(knobArea.removeFromLeft(knobWidth).reduced(padding));
+    masterSlider.setBounds(knobArea.removeFromLeft(knobWidth).reduced(padding));
+    reverbSlider.setBounds(knobArea.removeFromLeft(knobWidth).reduced(padding));
+
+    auto buttonWidth = buttonArea.getWidth() / numControls;
+
+    auto brightArea = (buttonArea.removeFromLeft(buttonWidth));
+    brightButton.setBounds(brightArea.reduced(25, 0));
 }
 
 void PanOFlexAudioProcessorEditor::setSliderParams(juce::Slider& slider)
