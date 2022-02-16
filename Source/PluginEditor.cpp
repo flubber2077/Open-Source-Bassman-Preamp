951406d8f93acd29c0fd95860c396db4f08e55ae
Dylan <61366167+flubber2077@users.noreply.github.com>
2022-02-15 22:47:42 -0500

    GUI update
    Literally all visual work is so frustrating. I know it looks bad but I cannot close in on how to rectify that.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,7 @@ PanOFlexAudioProcessorEditor::PanOFlexAudioProcessorEditor (PanOFlexAudioProcess
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (700, 220);
+    setSize (700, 250);
 
     setSliderParams(volumeSlider);
     setSliderParams(bassSlider);
@@ -25,6 +25,8 @@ PanOFlexAudioProcessorEditor::PanOFlexAudioProcessorEditor (PanOFlexAudioProcess
     setSliderParams(reverbSlider);
 
     addAndMakeVisible(brightButton);
+    brightButton.setColour(juce::ToggleButton::ColourIds::tickColourId, juce::Colours::black);
+    brightButton.setColour(juce::ToggleButton::ColourIds::tickDisabledColourId, juce::Colours::black);
 }
 
 PanOFlexAudioProcessorEditor::~PanOFlexAudioProcessorEditor()
@@ -39,31 +41,49 @@ void PanOFlexAudioProcessorEditor::paint (juce::Graphics& g)
     auto primaryColor = juce::Colour(253, 216, 53);
     auto pDarkColor = juce::Colour(197, 166, 0);
     auto secondaryColor = juce::Colour(109, 76, 65);
-
+    auto darkGrey = juce::Colour(100, 100, 100);
+    auto grey = juce::Colour(200, 200, 200);
+    auto lightGrey = juce::Colour(235, 235, 235);
+    auto black = juce::Colours::black;
     g.fillAll (primaryColor);
 
     auto area = getLocalBounds();
-    auto banner = area.removeFromTop(18);
+    auto banner = area.removeFromTop(30);
 
     g.setColour(pDarkColor);
     g.fillRect(banner);
 
-    g.setColour (juce::Colours::black);
-    g.setFont (22.0f);
+    auto buttonArea = area.removeFromTop(30);
+    g.setColour(primaryColor);
+    g.fillRect(buttonArea);
 
-    g.drawFittedText(TRANS("BASSMAN 5F6-A"), banner.removeFromLeft(200), juce::Justification::centred, 1);
+    g.setColour (black);
+    juce::Font mainComponentFont ("Haettenschweiler", 26.0f, juce::Font::plain);
+    g.setFont(mainComponentFont);
+
+    g.drawFittedText(TRANS("BASSMAN 5F6-A"), banner.removeFromLeft(200), juce::Justification::left, false);
+    banner.removeFromRight(5);
+    g.drawFittedText(TRANS("by Dylan Jordan"), banner.removeFromRight(200), juce::Justification::right, false);
 
 
     auto textArea = area.removeFromBottom(40);
+    g.setColour(grey);
+    g.fillRect(textArea);
+
+    auto knobArea = area;
+    g.setColour(lightGrey);
+    g.fillRect(knobArea);
+
+    g.setColour(black);
 
     const auto width = area.getWidth() / numControls;
 
-    g.drawFittedText(TRANS("Volume"), textArea.removeFromLeft(width), juce::Justification::centred, 1);
-    g.drawFittedText(TRANS("Bass"), textArea.removeFromLeft(width), juce::Justification::centred, 1);
-    g.drawFittedText(TRANS("Middle"), textArea.removeFromLeft(width), juce::Justification::centred, 1);
-    g.drawFittedText(TRANS("Treble"), textArea.removeFromLeft(width), juce::Justification::centred, 1);
-    g.drawFittedText(TRANS("Output"), textArea.removeFromLeft(width), juce::Justification::centred, 1);
-    g.drawFittedText(TRANS("Reverb"), textArea.removeFromLeft(width), juce::Justification::centred, 1);
+    g.drawFittedText(TRANS("VOLUME"), textArea.removeFromLeft(width), juce::Justification::centred, false);
+    g.drawFittedText(TRANS("BASS"), textArea.removeFromLeft(width), juce::Justification::centred, false);
+    g.drawFittedText(TRANS("MIDDLE"), textArea.removeFromLeft(width), juce::Justification::centred, false);
+    g.drawFittedText(TRANS("TREBLE"), textArea.removeFromLeft(width), juce::Justification::centred, false);
+    g.drawFittedText(TRANS("OUTPUT"), textArea.removeFromLeft(width), juce::Justification::centred, false);
+    g.drawFittedText(TRANS("REVERB"), textArea.removeFromLeft(width), juce::Justification::centred, false);
 }
 
 void PanOFlexAudioProcessorEditor::resized()
@@ -71,7 +91,7 @@ void PanOFlexAudioProcessorEditor::resized()
     auto area = getLocalBounds();
     area.removeFromBottom(40);
 
-    auto bannerArea = area.removeFromTop(18);
+    auto bannerArea = area.removeFromTop(30);
     auto buttonArea = area.removeFromTop(30);
 
     auto knobArea = area;
@@ -99,7 +119,8 @@ void PanOFlexAudioProcessorEditor::setSliderParams(juce::Slider& slider)
     slider.setSliderStyle(juce::Slider::SliderStyle::Rotary);
     slider.setColour(juce::Slider::thumbColourId, secondaryColor);
     slider.setColour(juce::Slider::textBoxTextColourId, juce::Colours::black);
-    slider.setColour(juce::Slider::textBoxBackgroundColourId, juce::Colour(255, 255, 107));
+    slider.setColour(juce::Slider::textBoxBackgroundColourId, juce::Colour(245, 245, 250));
+    slider.setColour(juce::Slider::trackColourId, juce::Colour(109, 76, 65));
     slider.setTextBoxStyle(juce::Slider::TextBoxAbove, true, 50, 25);
     slider.setTextBoxIsEditable(true);
     addAndMakeVisible(slider);
